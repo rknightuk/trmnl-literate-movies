425ec1545eb28c42e405ed18d82a242b01c8bf0f
Robb Knight <desk@rknight.me>
2025-07-22 10:19:55 +0100

    Decades

--- public/movies.php
+++ public/movies.php
@@ -1,14 +1,19 @@
 <?php
 
 $grouped = json_decode(file_get_contents('../data/grouped.json'), true);
+$validDecades = ['1910','1920','1930','1940','1950','1960','1970','1980','1990','2000','2010','2020'];
 
-$decades = ['1910','1920','1930','1940','1950','1960','1970','1980','1990','2000','2010','2020'];
+$decades = $_GET['decades'] ?? [];
 
-$decade = $_GET['decade'] ?? $decades[array_rand($decades)];
-
-if (!in_array($decade, $decades)) {
-    $decade = $decades[array_rand($decades)];
+if (is_string($decades)) {
+    $decades = explode(',', $decades);
 }
+
+$decades = array_filter($decades, function($decade) use ($validDecades) {
+    return in_array($decade, $validDecades);
+});
+
+$decade = empty($decade) ? $validDecades[array_rand($validDecades)] : $decades[array_rand($decades)];
 
 $movies = $grouped['movies'][$decade] ?? [];
 
